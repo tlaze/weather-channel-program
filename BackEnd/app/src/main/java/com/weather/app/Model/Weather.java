1e9bc4fceb03df2ee9e57a80916878380be92766
mmccartneycs <102608673+mmccartneycs@users.noreply.github.com>
2023-03-22 10:53:36 -0400

    Basic weather

---
+++ BackEnd/app/src/main/java/com/weather/app/Model/Weather.java
@@ -0,0 +1,25 @@
+package com.weather.app.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Weather {
+    @Id
+    private long zip;
+    @Column
+    private String lat;
+    @Column
+    private String lng;
+    @Column
+    private String city;
+    @Column
+    private String state_id;
+    @Column
+    private String state_name;
+}
