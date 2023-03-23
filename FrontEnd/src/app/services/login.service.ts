7d7362720d4aa310503daa657dee01ac70a80a14
Sadrach <sadrach@Jesuss-MacBook-Air.local>
2023-03-23 10:26:00 -0700

    Changed endpoints

--- FrontEnd/src/app/services/login.service.ts
+++ FrontEnd/src/app/services/login.service.ts
@@ -15,14 +15,14 @@ export class LoginService {
     let header : HttpHeaders = new HttpHeaders();
     header.append("accept", "text/json");
     header.append("Access-Control-Allow-Origin", "*");
-    return this.http.post<UserLogin>("http://127.0.0.1:9000/login", user, {headers:header});
+    return this.http.post<UserLogin>("http://127.0.0.1:9000/account", user, {headers:header});
   }
 
   getUsers():Observable<UserLogin>{
     let header : HttpHeaders = new HttpHeaders();
     header.append("accept", "text/json");
     header.append("Access-Control-Allow-Origin", "*");
-    return this.http.get<UserLogin>("http://127.0.0.1:9000/login", {headers:header});
+    return this.http.get<UserLogin>("http://127.0.0.1:9000/account", {headers:header});
   }
 
 }
