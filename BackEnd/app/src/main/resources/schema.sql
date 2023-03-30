21b71f3bf557c4b68f0097391988b187ef77e404
mmccartneycs <102608673+mmccartneycs@users.noreply.github.com>
2023-03-30 16:08:05 -0400

    added favorites

--- BackEnd/app/src/main/resources/schema.sql
+++ BackEnd/app/src/main/resources/schema.sql
@@ -6,12 +6,15 @@ CREATE TABLE Account (
   logged_in BOOLEAN
 );
 CREATE TABLE Location (
-  zip int PRIMARY KEY,
+  locationid INT PRIMARY KEY AUTO_INCREMENT,
+  zip int,
   lat VARCHAR(255),
   lng VARCHAR(255),
   city VARCHAR(255),
   state_id VARCHAR(255),
   state_name VARCHAR(255),
   accountid INT,
+  favorites boolean,
   FOREIGN KEY (accountid) REFERENCES Account(id)
+
 );
