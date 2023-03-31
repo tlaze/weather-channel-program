d370248ced1a09f5d707b21379d59426018e2fdc
sadrachm <sadrachmart@gmail.com>
2023-03-30 17:58:56 -0700

    Big Screen Favorite & History in home

--- FrontEnd/src/app/component/locations/locations.component.ts
+++ FrontEnd/src/app/component/locations/locations.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Location } from 'src/app/models/location.module';
 
 @Component({
@@ -11,7 +12,21 @@ export class LocationsComponent {
   userLocations: Location[] = [];
   @Input()
   locationType: string="";
+    
+  convert(value: number|undefined) : number {
+    return value as number;
+  }
+  constructor(private router:Router){}
   
+  onSubmit(lat:number|undefined, lon:number|undefined) {
+    console.log(lat);
+    console.log(lon);
+    this.router.navigate(['/weather'], {queryParams: {
+      lat: this.convert(lat),
+      lon: this.convert(lon)
+    }});
+
+  }
 
   ngOnChanges() {
     console.log(this.userLocations)
--- FrontEnd/src/app/models/location.module.ts
+++ FrontEnd/src/app/models/location.module.ts
@@ -2,6 +2,7 @@ export interface Location {
     zip?:number,
     lat?:number,
     lon?:number,
+    lng?:number
     city?:String,
     state_id?:String,
     state_name?:String,
