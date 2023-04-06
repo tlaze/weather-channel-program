5cc18142fd9e4586bffb340f900b6ca07b820662
tlaze <tomlazore@gmail.com>
2023-04-06 12:40:16 -0400

    changed cors

--- BackEnd/app/src/main/java/com/weather/app/Controller/LocationController.java
+++ BackEnd/app/src/main/java/com/weather/app/Controller/LocationController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins={"http://localhost:4200", "http://localhost:9000"}, allowCredentials = "true")
+@CrossOrigin("*")
 @RestController
 public class LocationController {
     LocationService locationService;
