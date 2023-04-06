3168cfea7bb4141136a4c45634141ffff635698a
sadrachm <sadrachmart@gmail.com>
2023-04-05 19:38:46 -0700

    EC2 test start

---
+++ BackEnd/app/target/classes/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Account (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(255),
+  password VARCHAR(255),
+  account_created BOOLEAN,
+  logged_in BOOLEAN
+);
+CREATE TABLE Location (
+  locationid int PRIMARY KEY AUTO_INCREMENT ,
+  zip int,
+  lat VARCHAR(255),
+  lon VARCHAR(255),
+  city VARCHAR(255),
+  state_id VARCHAR(255),
+  state_name VARCHAR(255),
+  accountid INT,
+  favorites boolean,
+  FOREIGN KEY (accountid) REFERENCES Account(id)
+
+);
